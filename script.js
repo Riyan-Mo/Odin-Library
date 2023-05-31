64e129434e43f293894713826ae3af87ad2785fa
Riyan911 <riyan8308151899@gmail.com>
2023-05-31 20:25:11 +0530

    Added local storage

--- script.js
+++ script.js
@@ -1,54 +1,108 @@
-const addBook = document.querySelector("#addBook");
+const addBook = document.querySelector("#enterBook");
 const bookName = document.querySelector('#name');
 const bookAuthor = document.querySelector('#author');
 const bookPages = document.querySelector('#pages');
 const bookContainer = document.querySelector('#bookContainer');
+const bookIsRead = document.querySelector("#isRead");
+
+let count = 0;
 
 let books = [];
 
-function Book(name="Book Name", author="Author", pages="Pages"){
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
+window.onload = function(){
+ books = JSON.parse(localStorage.getItem("Books")) || []; 
+ renderBooks();
+}
+class Book{
+  constructor(_name, _author, _pages, _isRead){
+    this.name = _name;
+    this.author = _author;
+    this.pages = _pages;
+    this.isRead = _isRead; 
+  }
 }
 
-addBook.addEventListener('click', function(){
+addBook.addEventListener('submit', function(e){
+  e.preventDefault();
+  const book = getBookDetails();
+  books.push(book);
+  localStorage.setItem("Books", JSON.stringify(books));
+  renderBooks();
+})
+
+function getBookDetails(){
   let bookNm = bookName.value;
   let bookAth = bookAuthor.value;
   let bookPg = bookPages.value;
-  let book = new Book(bookNm, bookAth, bookPg);
-  addBookToLibrary(book);
-})
+  let isRead = bookIsRead.checked;
+  let book = new Book(bookNm, bookAth, bookPg, isRead);
+  return book;
+}
 
-function addBookToLibrary(literature){
-  books.push(literature);
-  let newBook = document.createElement('div');
-  for(let key in literature){
-    const attribute = document.createElement('p');
-    attribute.textContent = literature[key];
-    newBook.appendChild(attribute);
-  }
+function createReadBtn(newBook){
   let readBtn = document.createElement('button');
   readBtn.textContent = 'Read';
-  readBtn.className = 'read-button';
-  readBtn.addEventListener('click', function(){
-  if(readBtn.classList.contains('unread-button')){
-    readBtn.classList.remove('unread-button');
-    newBook.classList.remove('unread-book');
-  }
-  else{
-    readBtn.classList.add('unread-button');
-    newBook.classList.add('unread-book');
-  }
+  readBtn.classList.add('read-button');
+  readBtn.addEventListener('click', () => {
+    changeReadStatus(readBtn, newBook)
   })
-  newBook.appendChild(readBtn);
+  return readBtn;
+}
+
+function createDeleteBtn(newBook){
   let deleteBtn = document.createElement('button');
   deleteBtn.textContent = 'Delete';
   deleteBtn.className = 'delete-button';
-  newBook.appendChild(deleteBtn);
-  newBook.className = 'book';
   deleteBtn.addEventListener('click', function(){
+    books.splice(newBook.id, 1)
+    localStorage.setItem("Books", JSON.stringify(books));
     bookContainer.removeChild(newBook);
   })
-  bookContainer.appendChild(newBook);
+  return deleteBtn;
+}
+
+function renderBooks(){
+  bookContainer.innerHTML = "";
+  books.forEach(book => {
+      let newBook = document.createElement('div');
+      newBook.setAttribute("id", count);
+      count++;
+      bookDetails(book, newBook);
+      readBtn = createReadBtn(newBook);
+      newBook.appendChild(readBtn);
+      deleteBtn = createDeleteBtn(newBook);
+      newBook.appendChild(deleteBtn);
+      newBook.classList.add("book");
+      if(!book.isRead){
+          newBook.classList.add("unread-book");
+          readBtn.classList.add("unread-button");
+        }
+      bookContainer.append(newBook);      
+  })
+}
+
+function bookDetails(literature, newBook){
+  for(let key in literature){
+    if(key !== "isRead"){
+    const attribute = document.createElement('p');
+    attribute.textContent = literature[key];
+    newBook.appendChild(attribute);
+    }
+  }
+}
+
+function changeReadStatus(readBtn, newBook){
+  const changedBookId = newBook.id;
+  const changedBook = books.splice(changedBookId, 1);
+  changedBook[0].isRead = !changedBook[0].isRead;
+  books = [...changedBook, ...books];
+  localStorage.setItem("Books", JSON.stringify(books));
+  if(changedBook[0].isRead){
+        readBtn.classList.remove('unread-button');
+        newBook.classList.remove('unread-book');
+      }
+      else{
+        readBtn.classList.add('unread-button');
+        newBook.classList.add('unread-book');
+  }    
 }
